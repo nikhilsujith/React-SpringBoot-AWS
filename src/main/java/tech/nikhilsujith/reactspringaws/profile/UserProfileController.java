33ecc00267d31d05fa24145eb390122cfee7a074
Nikhil Sujith <niks@nikhils-mbp.myfiosgateway.com>
2021-01-06 16:20:25 -0500

    Part2: Connected frontend React with backend Spring with the help of react library axios.

--- src/main/java/tech/nikhilsujith/reactspringaws/profile/UserProfileController.java
+++ src/main/java/tech/nikhilsujith/reactspringaws/profile/UserProfileController.java
@@ -1,14 +1,19 @@
 package tech.nikhilsujith.reactspringaws.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.awt.*;
 import java.util.List;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("api/v1/user-profile")
+// React throws Cross origin error when accessing server on a different host.
+//This line "@CrossOrigin" says accept from anywhere
+@CrossOrigin("*")
 public class UserProfileController {
 
     private final UserProfileService userProfileService;
@@ -22,5 +27,20 @@ public class UserProfileController {
 //    method to return a list of users
     public List<UserProfile> getUserProfiles() {
         return userProfileService.getUserProfiles();
+    }
+
+
+    /*TODO: Take the following from the client and pass it to "userProfileService"
+     *   1. @PathVariable : Path Variable for the user profile ID: Used to assign image to a particular user
+     *   2. @RequestParam: The actual image which the user intends to upload*/
+    /*Image upload API*/
+    @PostMapping(
+            path = "{userProfileId}/image/download",
+            consumes = MediaType.MULTIPART_FORM_DATA_VALUE,
+            produces = MediaType.APPLICATION_JSON_VALUE
+    )
+    public void uploadUserProfileImage(@PathVariable("userProfileId") UUID userProfileId,
+                                       @RequestParam("file")MultipartFile file) {
+        userProfileService.uploadUserProfileImage(userProfileId, file);
     }
 }
--- src/main/java/tech/nikhilsujith/reactspringaws/profile/UserProfileService.java
+++ src/main/java/tech/nikhilsujith/reactspringaws/profile/UserProfileService.java
@@ -2,8 +2,10 @@ package tech.nikhilsujith.reactspringaws.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService  {
@@ -17,5 +19,16 @@ public class UserProfileService  {
 
     List<UserProfile> getUserProfiles() {
         return userProfileServiceDataAccessService.getUserProfiles();
+    }
+
+
+    /* Receive the userProfileId and file being sent from the client; (receiving from UserProfileController.java)*/
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+        /*TODO
+        *   1. Check if image is not empty
+        *   2. Check is file is an image
+        *   3. Whether user exists in database
+        *   4. If exists, grab some metadata from file if any
+        *   5. Store image in S3 and update database with S3 image and link*/
     }
 }
